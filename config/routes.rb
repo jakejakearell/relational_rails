34595eb2200ca15329bf6ce5038e5fff88716d89
Jacob Arellano <jmm.arellano@gmail.com>
2021-02-03 09:25:34 -0700

    Add form, view and route for new game entry

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
   get '/parents', to: 'parents#index'
+  get '/parents/new', to: "parents#new"
+  post '/parents', to: 'parents#create'
   get '/parents/:id', to: "parents#show"
 end
