9ac3bc93429717984de5786dd32642807821c5e3
Adam Bowers <adam.j.bowers@gmail.com>
2021-02-03 15:31:15 -0700

    Createdestory action and complete update action

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -23,4 +23,28 @@ class ParentsController < ApplicationController
 
     redirect_to '/parents'
   end
+
+  def edit
+    @game = Game.find(params[:id])
+    @video_store = VideoStore.find(params[:id])
+  end
+
+  def update
+    game = Game.find(params[:id])
+    game.update({
+      stadium_name: params[:game][:stadium_name],
+      attendance: params[:game][:attendance],
+      televised: params[:game][:televised]
+      })
+
+    game.save
+
+    redirect_to "/parents/#{game.id}"
+  end
+
+  def destroy
+    Game.destroy(params[:id])
+
+    redirect_to "/parents/"
+  end
 end
