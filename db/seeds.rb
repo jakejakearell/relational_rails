94158995ed753176bd56343b460a383d511b9839
Adam Bowers <adam.j.bowers@gmail.com>
2021-02-04 15:32:37 -0700

    Add seeds for parent tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Game.destroy_all
+VideoStore.destroy_all
+
+game_1 = Game.create!(televised: true, stadium_name: "MetLife", attendance: 30000)
+game_2 = Game.create!(televised: false, stadium_name: "Coors", attendance: 25000)
+
+video_store_1 = VideoStore.create!(name: "Video Plus", rank: 1, flagship_store: false)
+video_store_2 = VideoStore.create!(name: "Video Prime", rank: 2, flagship_store: true)
