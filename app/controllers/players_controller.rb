e46c3977c05ccc7de13d823e7ea1b17bc00556f4
Jacob Arellano <jmm.arellano@gmail.com>
2021-02-06 12:46:14 -0700

    Add route, action and view for creating players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -7,6 +7,22 @@ class PlayersController < ApplicationController
     @player = Player.find(params[:id])
   end
 
+  def new
+    @game = Game.find(params[:id])
+  end
+
+  def create
+    player = Player.new({
+      position: params[:player][:position],
+      injured: params[:player][:injured],
+      weight: params[:player][:weight],
+      game_id: params[:id]
+      })
+
+    player.save
+    redirect_to "/games/#{player.game.id}/players"
+  end
+
   def edit
     @player = Player.find(params[:id])
   end
