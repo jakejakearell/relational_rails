08746d3f6799ea9f2e3528e6e1278fe3bf7eeeb9
Adam Bowers <adam.j.bowers@gmail.com>
2021-02-07 10:59:05 -0700

    Update test for #edit

--- spec/features/games/edit_spec.rb
+++ spec/features/games/edit_spec.rb
@@ -1,27 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "games edit page" do
-  # it "can display form to edit a game" do
-  #   game = Game.create!(televised: false,
-  #                       stadium_name: "Wriggley",
-  #                       attendance: 20000)
-  #
-  #   visit "/games/#{game.id}/edit"
-  #
-  #   expect(page).to find_field('stadium name:').value
-  # end
-
-  it "can have forms filled out" do
+  it "can display form to edit a game" do
     game = Game.create!(televised: false,
                         stadium_name: "Wriggley",
                         attendance: 20000)
 
     visit "/games/#{game.id}/edit"
-
-    fill_in 'stadium_name', :with => 'MetLife'
-    # fill_in('attendance', with: '30000')
-    # fill_in('televised', with: 'true')
-
-    save_and_open_page
+    fill_in 'game[stadium_name]', with: 'MetLife'
+    fill_in 'game[attendance]', with: 500
+    fill_in 'game[televised]', with: true
+    click_button
+    expect(page).to have_content("MetLife")
+    expect(page).to have_content(500)
+    expect(page).to have_content(true)
   end
 end
