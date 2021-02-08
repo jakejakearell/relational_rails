6040d8d56b850e07f699df96e23db694f9a38a9b
Jacob Arellano <jmm.arellano@gmail.com>
2021-02-08 08:01:17 -0700

    Start writing tests to more closely mimic user stories

--- spec/features/games/index_spec.rb
+++ spec/features/games/index_spec.rb
@@ -1,23 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe "games index page" do
-  it "can see all game stadium name, attendance and whether televised" do
-    game_1 = Game.create!(televised: false,
-                          stadium_name: "Wriggley",
-                          attendance: 20000)
-    game_2 = Game.create!(televised: true,
-                          stadium_name: "Mile High",
-                          attendance: 30000)
+RSpec.describe "As a vistor" do
+  describe "I visit /games" do
+    it "Then I see the name of each game record in the system" do
+      game_1 = Game.create!(televised: false,
+                            stadium_name: "Wriggley",
+                            attendance: 20000)
+      game_2 = Game.create!(televised: true,
+                            stadium_name: "Mile High",
+                            attendance: 30000)
 
-    visit '/games'
+      visit '/games'
 
-    expect(page).to have_content(game_1.stadium_name)
-    expect(page).to have_content(game_2.stadium_name)
+      expect(page).to have_content(game_1.stadium_name)
+      expect(page).to have_content(game_2.stadium_name)
+    end
   end
 
-  it "can display new game link" do
-    visit '/games'
+  describe "I visit '/parents/:id'" do
+    it "can display new game link" do
+      visit '/games'
 
-    expect(page).to have_link 'new game', href: "/games/new"
+      expect(page).to have_link 'new game', href: "/games/new"
+    end
   end
 end
