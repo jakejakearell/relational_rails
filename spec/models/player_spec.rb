fa51734e4a6de08c3409da8d43ddb8f0c242c113
Adam Bowers <adam.j.bowers@gmail.com>
2021-02-09 18:55:28 -0700

    Add test skeleton for #injured?

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -4,4 +4,35 @@ describe Player, type: :model do
   describe 'relationships' do
     it { should belong_to :game }
   end
+
+  describe 'instance methods' do
+    describe '#injured?' do
+      it 'returns only players that are injured to the child index' do
+        game_1 = Game.create!(televised: false,
+                              stadium_name: "Wriggley",
+                              attendance: 20000)
+        game_2 = Game.create!(televised: false,
+                              stadium_name: "Coors",
+                              attendance: 25000)
+
+        player_1 = game_1.players.create!(position: "QB", weight: 210, injured: true)
+        player_2 = game_1.players.create!(position: "RB", weight: 180, injured: false)
+        player_3 = game_2.players.create!(position: "DB", weight: 195, injured: true)
+
+        # params = {id: }
+
+        require "pry"; binding.pry
+      end
+    end
+  end
 end
+
+
+
+# def self.injured?(params)
+#   Player.where("game_id = ? AND injured = ?", params[:id], true)
+# end
+#
+# def self.weight?(params)
+#   Player.where("game_id = ? AND weight > ?", params[:id], params[:query] )
+# end
