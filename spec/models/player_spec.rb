ed0b106ee1ea92998f007a0113f6552e72b1bfbd
Adam Bowers <adam.j.bowers@gmail.com>
2021-02-09 20:20:10 -0700

    Complete #weight? test

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -27,15 +27,32 @@ describe Player, type: :model do
         expect(Player.injured?(params)).to eq(expected)
       end
     end
+
+    describe '#weight?' do
+      it "returns only players that are above a weight entered by the user with results in the game's show page" do
+        game_1 = Game.create!(televised: false,
+                              stadium_name: "Wriggley",
+                              attendance: 20000)
+        game_2 = Game.create!(televised: false,
+                              stadium_name: "Coors",
+                              attendance: 25000)
+
+        player_1 = game_1.players.create!(position: "QB", weight: 210, injured: true)
+        player_2 = game_1.players.create!(position: "RB", weight: 180, injured: false)
+        player_3 = game_1.players.create!(position: "DB", weight: 195, injured: true)
+        player_4 = game_2.players.create!(position: "DB", weight: 195, injured: true)
+
+        params = {id: game_1.id, query: 190}
+
+        expected = [player_1, player_3]
+
+        expect(Player.weight?(params)).to eq(expected)
+      end
+    end
   end
 end
 
 
-
-# def self.injured?(params)
-#   Player.where("game_id = ? AND injured = ?", params[:id], true)
-# end
-#
 # def self.weight?(params)
 #   Player.where("game_id = ? AND weight > ?", params[:id], params[:query] )
 # end
