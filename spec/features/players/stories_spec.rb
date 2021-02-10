63193d1cd6d32b8768e68ad25a8f40822193eb3f
Jacob Arellano <jmm.arellano@gmail.com>
2021-02-10 16:57:35 -0700

    Add test for user story 20

--- spec/features/players/stories_spec.rb
+++ spec/features/players/stories_spec.rb
@@ -161,4 +161,16 @@ RSpec.describe "As a vistor" do
       expect(current_path).to eq("/players/#{player_1.id}/edit")
     end
   end
+
+  describe "I visit the `game players` index page" do
+    it "has a link to edit that child's info" do
+      game_1 = Game.create!(televised: false,
+        stadium_name: "Wriggley",
+        attendance: 20000)
+      player_1 = game_1.players.create!(position: "QB", weight: 210, injured: true)
+      visit "/games/#{game_1.id}/players"
+      click_link 'edit player'
+      expect(current_path).to eq("/players/#{player_1.id}/edit")
+    end
+  end
 end
