e92cf4c305ce1a508b47cc00ce62c311dc7d1b52
Adam Bowers <adam.j.bowers@gmail.com>
2021-02-10 08:51:27 -0700

    Add #number_of_movies

--- app/models/video_store.rb
+++ app/models/video_store.rb
@@ -4,4 +4,8 @@ class VideoStore < ApplicationRecord
   def self.order_by_created_date
     VideoStore.order(created_at: :desc)
   end
+
+  def self.number_of_movies(video_store_id)
+    VideoStore.joins("INNER JOIN movies ON movies.video_store_id = video_stores.id").where("video_stores.id = ?", video_store_id).count
+  end
 end
