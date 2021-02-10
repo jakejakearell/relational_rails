5d583e29caead4a1f87b60aaf5aa46b66e28591d
Jacob Arellano <jmm.arellano@gmail.com>
2021-02-09 19:46:51 -0700

    Add test for user story 18

--- spec/features/games/index_spec.rb
+++ spec/features/games/index_spec.rb
@@ -24,4 +24,20 @@ RSpec.describe "As a vistor" do
       expect(page).to have_link 'new game', href: "/games/new"
     end
   end
+
+  describe "I visit '/parents/:id'" do
+    it "displays link to edit story" do
+      game_1 = Game.create!(televised: false,
+        stadium_name: "Wriggley",
+        attendance: 20000)
+      game_2 = Game.create!(televised: true,
+        stadium_name: "Mile High",
+        attendance: 30000)
+
+      visit '/games'
+
+      expect(page).to have_link 'update game', href: "/games/#{game_1.id}/edit"
+      expect(page).to have_link 'update game', href: "/games/#{game_2.id}/edit"
+    end
+  end
 end
