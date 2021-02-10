77602549040c3d81be026a3e7381b7ea71336540
Adam Bowers <adam.j.bowers@gmail.com>
2021-02-10 08:51:49 -0700

    Add #number_of_movies test

--- spec/models/video_store_spec.rb
+++ spec/models/video_store_spec.rb
@@ -16,7 +16,7 @@ describe VideoStore, type: :model do
                                           rank: 2,
                                           flagship_store: false,
                                           created_at: "Tues Jun 03 12:35:19 -0700 2008")
-        video_store_3 = VideoStore.create!(name: "Video 2",
+        video_store_3 = VideoStore.create!(name: "Video 3",
                                           rank: 2,
                                           flagship_store: false,
                                           created_at: "Wed Jun 04 12:35:19 -0700 2008")
@@ -26,5 +26,25 @@ describe VideoStore, type: :model do
         expect(VideoStore.order_by_created_date).to eq(expected)
       end
     end
-  end 
+
+    describe '#number_of_movies' do
+      it "returns the number of movies in the video store's show page" do
+        video_store_1 = VideoStore.create!(name: "Video 1",
+                                          rank: 1,
+                                          flagship_store: true)
+        video_store_2 = VideoStore.create!(name: "Video 2",
+                                          rank: 2,
+                                          flagship_store: false)
+
+        movie_1 = video_store_1.movies.create!(name:"Rent", available: true, year_filmed: 2001)
+        movie_2 = video_store_1.movies.create!(name:"Rambo", available: false, year_filmed: 1989)
+        movie_3 = video_store_1.movies.create!(name:"Alien", available: false, year_filmed: 1988)
+        movie_4 = video_store_2.movies.create!(name:"Predator", available: true, year_filmed: 1990)
+
+        video_store_id = video_store_1[:id]
+
+        expect(VideoStore.number_of_movies(video_store_id)).to eq(3)
+      end
+    end
+  end
 end
