236ee4480ba9e014438121468024746b0efc24cf
Adam Bowers <adam.j.bowers@gmail.com>
2021-02-10 12:38:54 -0700

    Test for #available?

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ApplicationRecord
-  belongs_to :video_store 
+  belongs_to :video_store
+
+  def self.available?(params)
+    Movie.where("video_store_id = ? AND available = ?", params[:id], true)
+  end
 end
