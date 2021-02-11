5e0fbc3f5500bd474fd5e056597e4e9834976303
Jacob Arellano <jmm.arellano@gmail.com>
2021-02-10 17:38:22 -0700

    Add test for user story 21

--- spec/features/players/stories_spec.rb
+++ spec/features/players/stories_spec.rb
@@ -185,7 +185,26 @@ RSpec.describe "As a vistor" do
 
       expect(page).to have_link 'Delete', href: "/players/#{player_1.id}"
 
-      click_link 'Delete'
+      click_link 'Delete', href: "/players/#{player_1.id}"
+
+      expect(current_path).to eq("/players/")
+
+      expect(page).to_not have_content(game_1.stadium_name)
+    end
+  end
+
+  describe "I visit the `players` index page" do
+    it "has a link to delete that child's info" do
+      game_1 = Game.create!(televised: false,
+        stadium_name: "Wriggley",
+        attendance: 20000)
+      player_1 = game_1.players.create!(position: "QB", weight: 210, injured: true)
+
+      visit "games/#{game_1.id}/players"
+
+      expect(page).to have_link 'Delete', href: "/players/#{player_1.id}"
+
+      click_link 'Delete', href: "/players/#{player_1.id}"
 
       expect(current_path).to eq("/players/")
 
